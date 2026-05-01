967bda5b678ad17a67aadd8015c47cd1a771292a
Radha Vallabh <rvallabh.devtest@gmail.com>
2026-05-01 12:03:12 +0530

    Add CSRF trusted origins for deployment

--- djfintracker/settings.py
+++ djfintracker/settings.py
@@ -124,3 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
+CSRF_TRUSTED_ORIGINS = [
+    "https://financetracker-ximt.onrender.com",
+]
